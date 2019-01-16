bb289ef393bd2de77515ed0f34a14b81ca96bccd
NickAire <ronn03@uqo.ca>
2019-01-16 14:50:12 -0500

    JSON vignette fetch completed

---
+++ content/en/vignettes.js
@@ -0,0 +1,42 @@
+var myObj={
+	"vignette": [{
+			"title": "Find a job",
+			"url": "https://www.jobbank.gc.ca/findajob",
+			"descriptor": "Job Alerts is a free email notification service that sends you job postings twice a day. This is a fast and easy job search tool that anyone can use. All you need is an email address!",
+			"relevance": [
+				{"audience": [5, 1, 4, 5, 3, 2]},
+				{"objective": [1, 3, 5]},
+				{"goal": [5, 1, 1]}
+			],
+			"val": 0				
+		},
+
+		{
+			"title": "Starting a business",
+			"url": "https://www.canada.ca/en/services/business/start.html",
+			"descriptor": "The steps to consider before operating a business in Canada.",
+			"relevance": [
+				{"audience": [1, 1, 1, 3, 2, 4]},
+				{"objective": [1, 1, 1]},
+				{"goal": [1, 1, 1]}
+			],
+			"val": 0
+		},
+
+		{
+			"title": "My immigration or citizenship application",
+			"url": "https://www.canada.ca/en/immigration-refugees-citizenship/services/application.html",
+			"descriptor": "E",
+			"relevance": [{"audience": [5, 1, 1, 5, 3, 1]
+				},
+				{
+					"objective": [5, 1, 1]
+				},
+				{
+					"goal": [5, 1, 1]
+				}
+			],
+			"val": 0
+		}
+	]
+};
